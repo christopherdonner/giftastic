cff981af88986d976abc36e558508fb4cb6ce3d7
Christopher Donner <christopher.donner@gmail.com>
2018-12-18 15:25:11 -0500

    ajax query returns 200

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,7 +1,14 @@
 var topics=[]
-var query=""
+var query="cats"
+var rating="g"
 var apiKey="PWJVgwxjQJNaf0VkGGCRU10EJah7ZjSP"
-var queryURL=`https://api/v1/gifs/search/api-key=?${apiKey}&q=${query}`
+var queryURL=`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`
 
 console.log("i'm running")
-
+console.log(queryURL)
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });

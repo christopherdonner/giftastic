bbeed4e77033747823fd1c464c5f02cbf73bbb5a
christopherdonner <christopher.donner@gmail.com>
2018-12-19 21:58:52 -0500

    progress

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,14 +1,28 @@
-var topics=[]
-var query="cats"
+var topics=["cat", "dog", "bat", "cow"]
+var query=""
 var rating="g"
 var apiKey="PWJVgwxjQJNaf0VkGGCRU10EJah7ZjSP"
 var queryURL=`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`
+var button=$("<button>")
+var div=$("<div>")
+var p = $("<p>")
 
-console.log("i'm running")
+for(i=0;i<topics.length;i++)
+{
+  console.log(topics[i])
+  div.appendTo($("#main")).attr("id", i)
+  button.text(topics[i]).addClass("btn btn-primary").attr("id", i).appendTo(div)
+  //btn.appendTo(p).appendTo($("#main"))
+}
+
+
 console.log(queryURL)
-$.ajax({
+$.ajax(
+  {
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function(response) 
+  {
     console.log(response);
   });
+

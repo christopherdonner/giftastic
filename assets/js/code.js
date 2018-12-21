f69a4723650317422866a7b69f300123f7cd0b08
Christopher Donner <christopher.donner@gmail.com>
2018-12-21 11:54:05 -0500

    image onclick working :

--- assets/js/code.js
+++ assets/js/code.js
@@ -6,7 +6,8 @@ var queryURL="https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"q="+query
 var button=$("<button>")
 var div=$("<div>")
 var p = $("<p>")
-var main=$("#main")
+//var main=$("#main")
+var image=$("<img>")
 
 for(i=0;i<topics.length;i++)
 {
@@ -22,7 +23,8 @@ for(i=0;i<topics.length;i++)
   }
 }
 
-$("button").on("click", function() {
+$("button").on("click", function() 
+{
 console.log(this.id)
 query=this.id
 console.log(queryURL)
@@ -33,7 +35,7 @@ $.ajax(
   }).then(function(response) 
   {
     console.log(response.data[0].images);
-    var image=$("<img>")
+    
     image.addClass("pictures")
     image.attr("src", response.data[0].images.fixed_height_still.url)
     $("#gifs").append(image)
@@ -42,5 +44,10 @@ $.ajax(
     image.attr("data-state", "still")
   });
 
-
+image.on("click", function()
+{
+  console.log("pic click")
+  console.log($(this).attr("data-state"))
+  //if()
+})
 })

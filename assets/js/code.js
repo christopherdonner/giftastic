94206d175e80049cfcacdd2bbba98d52c9658c26
christopherdonner <christopher.donner@gmail.com>
2018-12-22 07:08:59 -0500

    all features exist

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,4 +1,4 @@
-var topics=["cat", "dog", "bat", "cow"]
+var topics=["cat", "dog", "bat", "cow", "rabbit"]
 var query=""
 var rating="g"
 var apiKey="PWJVgwxjQJNaf0VkGGCRU10EJah7ZjSP"
@@ -7,15 +7,15 @@ var button=$("<button>")
 var div=$("<div>")
 var p = $("<p>")
 //var main=$("#main")
-var image=$("<img>")
+var image=$("<img>") // if image is declared globally, the append fails (???)
 
+//function makeButtons(){}
 for(i=0;i<topics.length;i++)
 {
   for (var i = 0; i < topics.length; i++) 
   {
-
    var btn = $("<button>");
-    btn.attr("data-name", topics[i])
+    //btn.attr("data-name", topics[i])
     btn.text(topics[i])
     btn.addClass("btn btn-dark");
     btn.attr("id", topics[i])
@@ -35,19 +35,31 @@ $.ajax(
   }).then(function(response) 
   {
     console.log(response.data[0].images);
+    //var image=$("<img>")
     
-    image.addClass("pictures")
-    image.attr("src", response.data[0].images.fixed_height_still.url)
-    $("#gifs").append(image)
-    image.attr("data-still", response.data[0].images.fixed_height_still.url)
-    image.attr("data-animated", response.data[0].images.fixed_height.url)
-    image.attr("data-state", "still")
+    image.addClass("pictures");
+    image.attr("src", response.data[0].images.fixed_height_still.url);
+    image.attr("data-still", response.data[0].images.fixed_height_still.url);
+    image.attr("data-animated", response.data[0].images.fixed_height.url);
+    image.attr("data-state", "still");
+    $("#gifs").append(image);
   });
 
+//var gif=$(".pictures")
 image.on("click", function()
 {
-  console.log("pic click")
+  var state = $(this).attr("data-state")
+  console.log(this)
   console.log($(this).attr("data-state"))
-  //if()
+  if(state==="still")
+  {
+    image.attr("src", $(this).attr("data-animated"))
+    image.attr("data-state", "animated")
+  }
+  else
+  {
+    image.attr("src", $(this).attr("data-still"))
+    image.attr("data-state", "still")
+  }
 })
 })

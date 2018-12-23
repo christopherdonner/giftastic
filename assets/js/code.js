f0b55972aa418212beb1a9b0b1f26d0ce13cf6a2
christopherdonner <christopher.donner@gmail.com>
2018-12-22 19:38:22 -0500

    button add

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,65 +1,77 @@
-var topics=["cat", "dog", "bat", "cow", "rabbit"]
-var query=""
-var rating="g"
-var apiKey="PWJVgwxjQJNaf0VkGGCRU10EJah7ZjSP"
-var queryURL="https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"q="+query
-var button=$("<button>")
-var div=$("<div>")
+var topics = ["cat", "dog", "bat", "cow", "rabbit"]
+var query = ""
+var rating = "g"
+var apiKey = "PWJVgwxjQJNaf0VkGGCRU10EJah7ZjSP"
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "q=" + query
+var button = $("<button>")
+var div = $("<div>")
 var p = $("<p>")
+
 //var main=$("#main")
-var image=$("<img>") // if image is declared globally, the append fails (???)
+//var image=$("<img>") // if image is declared globally, the append fails (???)
 
 //function makeButtons(){}
-for(i=0;i<topics.length;i++)
-{
-  for (var i = 0; i < topics.length; i++) 
-  {
-   var btn = $("<button>");
-    //btn.attr("data-name", topics[i])
-    btn.text(topics[i])
-    btn.addClass("btn btn-dark");
-    btn.attr("id", topics[i])
-    $("#buttons").append(btn);
+function drawButtons() {
+  $("#buttons").empty();
+  for (i = 0; i < topics.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
+      var btn = $("<button>");
+      //btn.attr("data-name", topics[i])
+      btn.text(topics[i])
+      btn.addClass("btn btn-dark");
+      btn.attr("id", topics[i])
+      $("#buttons").append(btn);
+    }
   }
 }
 
-$("button").on("click", function() 
-{
-console.log(this.id)
-query=this.id
-console.log(queryURL)
-$.ajax(
-  {
-    url: "https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"&q="+query,
-    method: "GET"
-  }).then(function(response) 
-  {
-    console.log(response.data[0].images);
-    //var image=$("<img>")
-    
-    image.addClass("pictures");
-    image.attr("src", response.data[0].images.fixed_height_still.url);
-    image.attr("data-still", response.data[0].images.fixed_height_still.url);
-    image.attr("data-animated", response.data[0].images.fixed_height.url);
-    image.attr("data-state", "still");
-    $("#gifs").append(image);
-  });
+drawButtons();
 
-//var gif=$(".pictures")
-image.on("click", function()
-{
-  var state = $(this).attr("data-state")
-  console.log(this)
-  console.log($(this).attr("data-state"))
-  if(state==="still")
-  {
-    image.attr("src", $(this).attr("data-animated"))
-    image.attr("data-state", "animated")
-  }
-  else
-  {
-    image.attr("src", $(this).attr("data-still"))
-    image.attr("data-state", "still")
-  }
+//onclick event listener for the buttons
+$("button").on("click", function () {
+  console.log(this.id)
+  query = this.id
+  console.log(queryURL)
+  $.ajax(
+    {
+      url: "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + query,
+      method: "GET"
+    }).then(function (response) {
+      var rando=Math.floor(Math.random()*10)
+      console.log(response.data[rando].images);
+      var pic = $("<img>")
+
+      pic.attr("src", response.data[rando].images.fixed_height_still.url);
+      pic.attr("data-still", response.data[rando].images.fixed_height_still.url);
+      pic.attr("data-animated", response.data[rando].images.fixed_height.url);
+      pic.attr("data-state", "still");
+      pic.addClass("pictures");
+      $("#gifs").append(pic);
+    });
+
+    //var gif=$(".pictures")
+    //var image = $(".pictures")
+  $(".pictures").on("click", function () {
+    var state = $(this).attr("data-state")
+    console.log(this)
+    console.log($(this).attr("data-state"))
+    if ($(this).attr("data-state") === "still") {
+      $(this).attr("src", $(this).attr("data-animated"))
+      $(this).attr("data-state", "animated")
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"))
+      $(this).attr("data-state", "still")
+    }
+  })
 })
+
+// add new buttons from addButton form upon submitButton click
+$("#submitButton").on("click", function () {
+  event.preventDefault();
+  console.log($("#addButton").val())
+  topics.push($("#addButton").val())
+  $("#addButton").val("")
+  drawButtons();
+  //topics.push()
 })
